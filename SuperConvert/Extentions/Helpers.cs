377e2cd077984f7f00cb39395fba112ddb21cf40
Esam Elzain <esam@edw.ae>
2022-09-21 15:42:01 +0400

    Using System.Text.Json over Streams

--- SuperConvert/Extentions/Helpers.cs
+++ SuperConvert/Extentions/Helpers.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Runtime.Serialization.Json;
+using System.Text.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -79,25 +79,14 @@ namespace SuperConvert.Extentions
         /// </summary>
         private static T Deserialize<T>(string body)
         {
-            using (var stream = new MemoryStream())
-            using (var writer = new StreamWriter(stream))
-            {
-                writer.Write(body);
-                writer.Flush();
-                stream.Position = 0;
-                return (T)new DataContractJsonSerializer(typeof(T)).ReadObject(stream);
-            }
+            return JsonSerializer.Deserialize<T>(body);
         }
         /// <summary>
         /// Serialize an object to json
         /// </summary>
         private static string Serialize<T>(T item)
         {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                new DataContractJsonSerializer(typeof(T)).WriteObject(ms, item);
-                return Encoding.Default.GetString(ms.ToArray());
-            }
+            return JsonSerializer.Serialize<T>(item);
         }
     }
 }
