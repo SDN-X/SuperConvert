b085bef7e4da62192fc1393ea93ca8d05a7b4e05
Esam Elzain <esam@edw.ae>
2022-10-03 15:18:48 +0400

    Adding SafeDeserialize Extention

--- SuperConvert/Extentions/JsonDataTable.cs
+++ SuperConvert/Extentions/JsonDataTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Text.Json;
 
 namespace SuperConvert.Extentions
 {
@@ -16,6 +17,9 @@ namespace SuperConvert.Extentions
         public static DataTable ToDataTable(this string jsonString, string tableName = "") => string.IsNullOrEmpty(jsonString)
                 ? throw new ArgumentNullException("String value can not be empty or null!")
                 : Helpers.JsonToDataTable(jsonString, tableName);
+
+
+
         /// <summary>
         /// Converts from DataTable to List of object ,Returned Json will be List of objects 
         /// </summary>
@@ -25,5 +29,30 @@ namespace SuperConvert.Extentions
         /// <exception cref="Exception"></exception>
         public static string ToJson(this DataTable dataTable)
         => dataTable == null ? throw new ArgumentNullException("DataTable can not be empty or null!") : Helpers.DataTableToJson(dataTable);
+
+
+
+        /// <summary>
+        /// Safe deserializing json string to object
+        /// </summary>
+        /// <param name="json"></param>
+        /// <param name="returnedType"></param>
+        /// <param name="serializeOptions"></param>
+        /// <returns></returns>
+        public static object SafeDeserialize(this string json, Type returnedType, JsonSerializerOptions serializeOptions = null)
+        {
+            try
+            {
+                if (serializeOptions == null)
+                {
+                    serializeOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                }
+                return JsonSerializer.Deserialize(json, returnedType, serializeOptions);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
