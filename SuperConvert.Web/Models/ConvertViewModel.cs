b3a553876040ed7069f84f2ac565efe2649b043f
Esam Elzain <esam@edw.ae>
2023-01-09 10:17:03 +0400

    Move Web Project to another solution

--- SuperConvert.Web/Models/ConvertViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SuperConvert.Web.Models
-{
-    public class ConvertViewModel
-    {
-        public int ConvertType { get; set; }
-        public string Original { get; set; }
-        public string Converted { get; set; }
-    }
-}
